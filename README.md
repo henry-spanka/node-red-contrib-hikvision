7eccc5ddfd3ca8f048cd3152dffb9e6f9418ffe8
Henry Spanka <henry@spanka.eu>
2019-01-23 18:16:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# node-red-contrib-hikvision
+
+Node-Red plugin to communicate with Hikvision IP surveillance cameras.
+
+[![NPM](https://nodei.co/npm/node-red-contrib-hikvision.png?compact=true)](https://npmjs.org/package/node-red-contrib-hikvision)
+
+# Help
+If you have any questions or help please open an issue on the GitHub project page.
+
+# Contributing
+Pull requests are always welcome. If you have a device that is not supported yet please open an issue or open a pull request with
+your modifications.
+
+# License
+The project is subject to the MIT license unless otherwise noted. A copy can be found in the root directory of the project [LICENSE](LICENSE).
---
+++ hikvision.js
@@ -0,0 +1,146 @@
+module.exports = function(RED) {
+    "use strict";
+    var HikvisionAPI = require('node-hikvision-api');
+
+    function HikvisionCredentialsNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        let options = {
+            host: this.credentials.host,
+            port: this.credentials.port,
+            user: this.credentials.username,
+            pass: this.credentials.password,
+            log: false,
+        };
+
+        this.options = options;
+    }
+
+    HikvisionCredentialsNode.prototype.connect = function(node) {
+        var hikApi = null;
+
+        node.status({
+            fill: "red",
+            shape: "ring",
+            text: "disconnected"
+        });
+
+        if (this.options != null) {
+            hikApi = new HikvisionAPI.hikvision(this.options);
+            hikApi.on('error', function(err) {
+                node.error(err);
+                node.status({
+                    fill: "red",
+                    shape: "ring",
+                    text: "disconnected"
+                });
+            });
+
+            hikApi.on('connect', function(err) {
+                node.status({
+                    fill: "green",
+                    shape: "ring",
+                    text: "connected"
+                });
+            });
+
+        }
+
+        return hikApi;
+    }
+
+    RED.nodes.registerType("hikvision-credentials", HikvisionCredentialsNode, {
+        credentials: {
+            host: {
+                type: "text"
+            },
+            port: {
+                type: "text"
+            },
+            username: {
+                type: "text"
+            },
+            password: {
+                type: "password"
+            }
+        }
+    });
+
+    function HikvisionAlarmInNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        this.hikvision = RED.nodes.getNode(config.hikvision);
+
+        this.hikApi = this.hikvision.connect(node);
+
+        if (this.hikApi != null) {
+            // Monitor Camera Alarms
+            this.hikApi.on('alarm', function(code, action, index) {
+                let data = {
+                    'code': code,
+                    'action': action,
+                    'index': index,
+                    'time': new Date()
+                };
+
+                node.send({
+                    payload: data
+                });
+            });
+        } else {
+            node.error("Invalid credentials");
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "conf invalid"
+            });
+        }
+
+        this.on('close', function(done) {
+            node.hikApi.client.destroy();
+            done();
+        });
+    }
+
+    RED.nodes.registerType("hikvision-alarm-in", HikvisionAlarmInNode);
+
+    function HikvisionProfileOutNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        this.hikvision = RED.nodes.getNode(config.hikvision);
+
+        this.hikApi = this.hikvision.connect(node);
+
+        if (this.hikApi != null) {
+            this.on('input', function(msg) {
+                if (msg.payload.profile == 'day') {
+                    this.hikApi.dayProfile();
+                } else if (msg.payload.profile == 'night') {
+                    this.hikApi.nightProfile();
+                } else {
+                    node.error("Invalid profile");
+                    return null;
+                }
+
+                node.send(msg);
+            });
+        } else {
+            node.error("Invalid credentials");
+            node.status({
+                fill: "red",
+                shape: "ring",
+                text: "conf invalid"
+            });
+        }
+
+        this.on('close', function(done) {
+            node.hikApi.client.destroy();
+            done();
+        });
+    }
+
+    RED.nodes.registerType("hikvision-profile-out", HikvisionProfileOutNode);
+}
